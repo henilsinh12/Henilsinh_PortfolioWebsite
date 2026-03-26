24282262adba7e278f404101c49d1dcfff5d240a
henilsinh12 <24amtics089@gmail.com>
2026-03-26 09:54:58 +0530

    Enhance script with scrolling, navbar, and typing effects
    Added smooth scrolling for anchor links and dynamic navbar background change on scroll. Implemented typing effect for a text display.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,39 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Portfolio Website Loaded Successfully!');
 });
+
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+anchor.addEventListener("click", function(e){
+e.preventDefault();
+document.querySelector(this.getAttribute("href")).scrollIntoView({
+behavior: "smooth"
+});
+});
+});
+
+
+window.addEventListener("scroll", function(){
+let navbar = document.querySelector("nav");
+
+if(window.scrollY > 50){
+navbar.style.background = "#111";
+}else{
+navbar.style.background = "transparent";
+}
+});
+
+
+const text = "Web Developer | Programmer";
+let index = 0;
+
+function type(){
+if(index < text.length){
+document.getElementById("typing").innerHTML += text.charAt(index);
+index++;
+setTimeout(type,100);
+}
+}
+
+type();
