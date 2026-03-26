8a0b3c6f229c8b83920b5dd64188d4f5b0d36ccb
henilsinh12 <24amtics089@gmail.com>
2026-03-26 10:18:32 +0530

    Implement smooth transitions for anchor link clicks
    Added smooth page transition on anchor link clicks.

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,15 +16,12 @@ behavior: "smooth"
 
 window.addEventListener("scroll", function(){
 let navbar = document.querySelector("nav");
-
 if(window.scrollY > 50){
 navbar.style.background = "#111";
 }else{
 navbar.style.background = "transparent";
 }
 });
-
-
 const text = "Web Developer | Programmer";
 let index = 0;
 
@@ -35,5 +32,20 @@ index++;
 setTimeout(type,100);
 }
 }
-
 type();
+
+
+
+
+document.querySelectorAll("a").forEach(link => {
+link.addEventListener("click", function(e){
+const target = this.getAttribute("href");
+if(target.includes(".html")){
+e.preventDefault();
+document.body.style.opacity = "0";
+setTimeout(()=>{
+window.location = target;
+},300);
+}
+});
+});
